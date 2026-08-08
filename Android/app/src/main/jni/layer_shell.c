08145d93e22183f73b7da2d7c2dc5dc7dfa435eb
cakroni1580 <cakroni158@gmail.com>
2026-08-08 15:38:52 +0700

    Update layer_shell.c

--- Android/app/src/main/jni/layer_shell.c
+++ Android/app/src/main/jni/layer_shell.c
@@ -27,7 +27,17 @@
 #include <android/log.h>
 #include <stdlib.h>
 #include <string.h>
-
+/*
+ * CONTEXT:
+ * Keyboard focus policy dimiliki oleh compositor/server.
+ *
+ * layer-shell tidak mengubah keyboard focus secara langsung.
+ * Setelah layer surface benar-benar aktif, layer-shell meminta
+ * compositor untuk mengubah focus melalui API external ini.
+ */
+extern void keyboard_focus_update(
+        struct wayland_server *srv,
+        struct compositor_surface *surface);
 #define LOG_TAG "TrierarchLayerShell"
 
 #define LOGI(...) \
