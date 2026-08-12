e68f001d97c4e03880dac6cf7f6d689f7da15e16
cakroni1580 <cakroni158@gmail.com>
2026-08-12 15:53:56 +0700

    Rename compositor_get_work_area to trierarch_work_area

--- Android/app/src/main/jni/gtk-shell.c
+++ Android/app/src/main/jni/gtk-shell.c
@@ -722,7 +722,7 @@ bool gtk_shell_get_work_area(
         !area)
         return false;
 
-    if (!compositor_get_work_area(
+    if (!trierarch_work_area(
             surf->srv,
             NULL,
             area)) {
